a33292b7f31cfb4a138535d09aa15c936e92ff0b
Rkesh Roushan <rakeshroushan2341@gmail.com>
2023-02-21 02:49:59 +0530

    Update Mongoose

--- 11_Mongoose/app.js
+++ 11_Mongoose/app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
-const path = require('path');
 
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
